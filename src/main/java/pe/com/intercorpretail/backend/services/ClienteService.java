ee941601b968704c35e2b5d30937603d851a6ecb
Klambre <owcessar@gmail.com>
2019-03-22 22:26:28 -0500

    calcular fecha de muerte

--- src/main/java/pe/com/intercorpretail/backend/services/ClienteService.java
+++ src/main/java/pe/com/intercorpretail/backend/services/ClienteService.java
@@ -3,8 +3,12 @@ package pe.com.intercorpretail.backend.services;
 import java.math.BigDecimal;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.List;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -38,6 +42,7 @@ public class ClienteService {
 			kpiRespuesta.setPromedio(new BigDecimal(calculoMedia));
 
 			Double tmpDesviacion = new Double(0.0);
+			//No use lambda, debido a que no me permite utilizar esta operacion "+="
 			for (Cliente cliente : iListReturn) {
 				if (cliente.getC_edad() != null) {
 					tmpDesviacion += Math.pow((cliente.getC_edad() - calculoMedia), 2);
@@ -55,11 +60,19 @@ public class ClienteService {
 
 	public List<Cliente> listarCliente() {
 		List<Cliente> iListReturn = null;
-		SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		LocalDate ahora = LocalDate.now();
 		try {
-			iListReturn = clienteIMapper.findAll();
+			iListReturn = clienteIMapper.findAll();			
 			iListReturn.stream().forEach(item -> {
-				System.out.println(format.format(item.getC_fec_nav()));
+				LocalDate fechaNac = LocalDate.parse(sdf.format(item.getC_fec_nav()), fmt);				
+				Period periodo = Period.between(fechaNac, ahora);				
+				if (periodo.getYears() > 60) {
+					item.setC_fec_muerte(Date.from(ahora.plusMonths(1).atStartOfDay(ZoneId.systemDefault()).toInstant()));
+				}else {
+					item.setC_fec_muerte(new Date("19/02/2035"));
+				}				
 			});
 
 		} catch (Exception e) {
